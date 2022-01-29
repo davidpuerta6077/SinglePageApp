eb4fd9f24bf64558288c9f79337c629ea4c2bab7
David Puerta <davidpb.2094@gmail.com>
2022-01-29 05:57:48 -0500

    first commit

---
+++ src/templates/Header.js
@@ -0,0 +1,19 @@
+const Header = () => {
+    const view = `
+    <div class="Header-main">
+    <div class="Header-logo">
+        <h1>
+            <a href="/">100tifico.co</a>
+        </h1>
+    </div>
+    <dic class="Header-nav">
+        <a href="#/about/">
+            about
+        </a>
+    </dic>
+</div>
+    `;
+    return view
+};
+
+export default Header;
